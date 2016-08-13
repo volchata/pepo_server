df8f6400138f36c89b2dae34b1bd0fadab01d7a5
develas <gomoniuk.vadim@gmail.com>
2016-08-13 17:28:01 +0300

    login api

--- router.js
+++ router.js
@@ -5,11 +5,41 @@ var path = require('path');
 
 function setHandlers(server) {
 
-    server.get('/user/:login', function (req, res, next) {
-        res.send('Hello world.  Here will be user ' + req.params.name + ' json in future');
+    server.get('/user/:id/exist', function (req, res, next) {
+        res.send({exist: false});
         return next(false);
     });
 
+    server.get('/login', function (req, res, next) {
+    	
+        res.send({
+        	login: "true",
+        	tweets: fillHellow()
+        });
+        return next(false);
+    });
+
+    server.get('/user/:id/twitt', function (req, res, next) {
+       	var helloW = [],
+       		offset = req.query.offset || 0;
+    	helloW.length = req.query.limit || 10;
+
+        res.send({
+        	tweets: fillHellow(req.query.limit, req.query.offset)
+        });
+        return next(false);
+    });
+
+    function fillHellow (limit, offset){
+    	var helloW = [],
+    	offset =  offset || 0;
+    	helloW.length = limit || 10;
+
+    	return helloW.fill("Hello world").map((value, index) => {
+        		return value + '-' + (parseInt(offset) + index + 1);
+        })
+
+    }
 }
 
 module.exports.set = setHandlers;
