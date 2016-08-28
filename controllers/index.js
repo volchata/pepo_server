77ccf23be38abd4fadbd5a8c06dd936343c55dc2
sasha <sasha@localhost.localdomain>
2016-08-28 19:10:49 +0300

    router

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = {
-    currentUser: require('./mUser').currentUser,
     limitData: require('./mUser').limitData,
     users: require('./users'),
     user: require('./user'),
