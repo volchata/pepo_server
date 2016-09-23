4215f48f6824f2080504be193f9f89228dfd97e0
Фурсенко Александр <alex1@kimkfu.ru>
2016-09-24 02:50:20 +0300

    Извлекаются title страниц снапшотов + фикс профиля пользователя

--- controllers/snapshot.js
+++ controllers/snapshot.js
@@ -5,6 +5,7 @@ const conf = require('../conf');
 const path = require('path');
 const encoder = conf.get('snapshotEngineCmd') || 'wkhtmltoimage';
 const opts = conf.get('snapshotEngineOpts') || [];
+const titleExtractor = path.join( process.cwd(), 'libs', 'titleExtractor.sh' );
 
 function textEscapeForRE(text) {
     return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&').replace(/\n|\r|\n\r|\r\n/g, '');
@@ -16,6 +17,18 @@ var dir = '^' + textEscapeForRE( conf.get('storage:dir') + path.sep );
 var goodDst = new RegExp( dir, 'i');
 
 function createSnapshot(src, dst, cb) {
+    var title;
+    var imageReady;
+    function aggregator(error, stdout, stderr) {
+        if (error) {
+            console.error('Error was catched while converting to image: \n', error);
+            console.log('', stdout);
+            console.log('', stderr);
+        }
+        if ((title) && (imageReady)) {
+            cb(error, title);
+        }
+    }
     if (!(goodSrc.test(src)) ) {
         // throw new Error('Source for image should be a url:' + src);
         src = 'http://' + src;
@@ -27,13 +40,16 @@ function createSnapshot(src, dst, cb) {
         throw new Error('Callback should be a function');
     }
     forker(encoder, opts.concat(src, dst), {timeout: 10000}, (error, stdout, stderr)=>{
-        if (error) {
-            console.error('Error was catched while converting to image: \n', error);
-            console.log('', stdout);
-            console.log('', stderr);
-        }
-        cb(error, 'Title');
+        imageReady = true;
+        aggregator(error, stdout, stderr);
     });
+    forker(titleExtractor, [src], {timeout: 4000}, (error, stdout, stderr)=>{
+        if (!error) {
+            title = stdout;
+        }
+        aggregator(error, stdout, stderr);
+    });
+
 }
 
 module.exports = createSnapshot;
